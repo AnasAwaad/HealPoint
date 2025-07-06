270fb5415e71d301e481384ba2b7a532fbfc50b5
anas awaad <anas.shaban.awaad@gmail.com>
2025-07-06 20:31:56 +0300

    seperate js code to doctor setting file and update ui in settings

---
+++ HealPoint.Presentation/wwwroot/js/doctor-settings.js
@@ -0,0 +1,97 @@
+﻿
+let selectedSymptomIds = [];
+
+$(document).ready(function () {
+    // service click
+    $('.js-service-card').on('click', function () {
+        $('.js-service-card').removeClass('active');
+        $(this).addClass('active');
+
+        const id = $(this).data('service-id');
+        $('#SelectedServiceId').val(id);
+    });
+
+    // specialization click
+    $('.js-spec-card').on('click', function () {
+        $('.js-spec-card').removeClass('active');
+        $(this).addClass('active');
+
+        const id = $(this).data('spec-id');
+        $('#SelectedSpecializationId').val(id);
+        console.log(id)
+    });
+
+    // collect all selected symptoms
+    $('.js-symptom-card[data-selected="true"]').each(function () {
+        const id = $(this).data('symptom-id');
+        selectedSymptomIds.push(id);
+    });
+
+    // symptom click
+    $('.js-symptom-card').on('click', function () {
+        const card = $(this)
+        const id = card.data('symptom-id');
+        if (card.hasClass('active')) {
+            card.removeClass('active');
+            selectedSymptomIds = selectedSymptomIds.filter(s => s !== id);
+        } else {
+            card.addClass('active');
+            selectedSymptomIds.push(id);
+        }
+        console.log(selectedSymptomIds);
+    })
+
+
+    $('#btnUpdateService').on('click', function () {
+        const serviceId = parseInt($('#SelectedServiceId').val());
+        if (!serviceId) return showErrorSwal('Please select a service.');
+
+        $.ajax({
+            url: '/DoctorSettings/UpdateService',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                DoctorId: parseInt($('#DoctorId').val()),
+                SelectedServiceId: serviceId
+            }),
+            success: () => showSuccessSwal('Your service is updated successfully.'),
+            error: () => showErrorSwal('An error occurred while updating the service.')
+        });
+    });
+
+    $('#btnUpdateSpecialization').on('click', function () {
+
+        const specId = parseInt($('#SelectedSpecializationId').val());
+        if (!specId) return showErrorSwal('Please select a specialization.');
+
+
+        $.ajax({
+            url: '/DoctorSettings/UpdateSpecialization',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                DoctorId: parseInt($('#DoctorId').val()),
+                SelectedSpecializationId: specId
+            }),
+            success: () => showSuccessSwal('Your specialization is updated successfully.'),
+            error: () => showErrorSwal('An error occurred while updating the specialization.')
+        });
+
+    });
+
+
+    $('#btnUpdateSymptoms').on('click', function () {
+        if (selectedSymptomIds.length === 0) return showErrorSwal('Please select at least one symptom.');
+        $.ajax({
+            url: '/DoctorSettings/UpdateSymptoms',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                DoctorId: parseInt($('#DoctorId').val()),
+                SelectedSymptoms: selectedSymptomIds
+            }),
+            success: () => showSuccessSwal('Your symptoms are updated successfully.'),
+            error: () => showErrorSwal('An error occurred while updating the symptoms.')
+        });
+    });
+});
